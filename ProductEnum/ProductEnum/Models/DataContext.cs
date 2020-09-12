d27baa8078d9c1aa9b81b5b9028cd7d9d885a427
Rodolfo Heredia <a2019115883@estudiantes.upsa.edu.bo>
2020-09-12 11:56:34 -0400

    Creacion del mvc

---
+++ ProductEnum/ProductEnum/Models/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ProductEnum.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        { 
+        }
+
+        public System.Data.Entity.DbSet<ProductEnum.Models.Product> Products { get; set; }
+    }
+}
---
+++ ProductEnum/ProductEnum/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProductEnum.Models
+{
+    public enum CategoryType
+    {
+        Electronicos=10,
+        Alimenticios=20,
+        Moda=30,
+        Belleza=40,
+        Automotriz=50,
+        Libros=60,
+    }
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        [Display(Name = "Nombre del producto")]
+        [Required(ErrorMessage ="Debe ingresar el nombre del producto")]
+        public string Name { get; set; }
+        [Display(Name = "Categoria del producto")]
+        [Required(ErrorMessage = "Debe ingresar la categoria del producto")]
+        public CategoryType Category { get; set; }
+    }
+}
